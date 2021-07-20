07f98bda446c12c610e8fb6d33180e26ce6d8ba6
allendai1 <65299178+allendai1@users.noreply.github.com>
2021-07-19 22:24:45 -0400

    '.

--- src/popup.js
+++ src/popup.js
@@ -4,76 +4,105 @@ import { saveAs } from "file-saver";
 // import ytdl from "ytdl-core";
 import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
 import axios from "axios";
+import ytdl from "ytdl-core";
 
 let video_url = null;
 const regex = /https:\/\/www.youtube.com\/watch?/g;
 const ffmpeg = createFFmpeg({
 	corePath: "https://unpkg.com/@ffmpeg/core@0.10.0/dist/ffmpeg-core.js",
-	log: false,
+	log: true,
 });
 chrome.tabs.query(
 	{ active: true, windowId: chrome.windows.WINDOW_ID_CURRENT },
-	function (tabs) {
+	async function (tabs) {
 		if (tabs[0].url.match(regex)) {
 			video_url = tabs[0].url;
+			if(!ffmpeg.isLoaded()){
+				await ffmpeg.load();
+		
+			}
 			document
 				.getElementById("audio_download")
-				.addEventListener("click", get_audio);
+				.addEventListener("click", download_audio);
 			document
 				.getElementById("video_download")
-				.addEventListener("click", get_video);
+				.addEventListener("click", download_video);
+				document
+				.getElementById("video_audio_download")
+				.addEventListener("click", download_full);
+				
 		}
 	}
 );
-async function get_audio() {
+
+async function get_audio() { // returns blob object representing the audio
 	let args = new URLSearchParams({
 		yt_link: video_url,
 	});
-
+	let audio = null;
 	let url = new URL("http:localhost:5000/audio");
 	url.search = args;
-	fetch(url, {
-		method: "GET",
-	}).then((res) => {
-		res.blob().then(async (x) => {
-			let file1 = new File([x], "test.mp4");
-			saveAs(file1, "test.mp4");
-		});
-	});
+
+	let res = await axios({
+		method: "get",
+		url: url,
+		responseType: "blob",
+		onDownloadProgress:e=>{
+			console.log(e.loaded);
+
+			
+		
+			
+
+		}
+	})
+	return res.data;
+
+	
 }
-async function get_video() {
+
+async function get_video() { // returns blob object representing the video
 	let args = new URLSearchParams({
 		yt_link: video_url,
 	});
 	let url = new URL("http:localhost:5000/video");
+	let video = null;
 	url.search = args;
 
-	axios({
+	let res = await axios({
 		method: "get",
 		url: url,
-		responseType: 'arraybuffer'
-		
-	}).then(async (x) => {
-		console.log(x)
-		await ffmpeg.load();
-		// let buffer1 = await x.arrayBuffer();
-		ffmpeg.FS("writeFile", "test.mp4", new Uint8Array(x.data));
-		let gg1 = ffmpeg.FS("readFile", "test.mp4");
-		// console.log(gg1);
-		saveAs(new File([gg1], "test.mp4"), "test.mp4");
-	});
-	// fetch(url, {
-	// 	method: "GET",
-	// }).then((res) => {
-	// 	res.blob().then(async (x) => {
-	// 		await ffmpeg.load();
-	// 		let buffer1 = await x.arrayBuffer();
-	// 		ffmpeg.FS("writeFile", "test.mp4", new Uint8Array(buffer1));
-	// 		// await ffmpeg.run("-i", "test.mp4", "test.mp3");
-	// 		// let gg = ffmpeg.FS("readFile", "test.mp4");
-	// 		let gg1 = ffmpeg.FS("readFile", "test.mp4");
-	// 		console.log(gg1);
-	// 		saveAs(new File([gg1], "test.mp4"), "test.mp4");
-	// 	});
-	// });
+		responseType: "blob",
+	})
+
+	
+	// video = ffmpeg.FS("readFile", "video.mp4");
+	
+	return res.data;
+}
+
+async function download_video(){
+	let video = await get_video();
+	saveAs(new File([video], "video.mp4"), "video.mp4");
+
+}
+async function download_audio(){
+	let audio = await get_audio();
+	console.log(audio)
+	saveAs(new File([audio], "audio.mp4"), "audio.mp4");
+}
+async function download_full(){
+	let video = new Uint8Array(await get_video().then(video=>video.arrayBuffer()));
+
+	let audio = new Uint8Array(await get_audio().then(audio=>audio.arrayBuffer()));
+
+	ffmpeg.FS("writeFile", "video.mp4", video );
+	ffmpeg.FS("writeFile", "audio.mp4", audio );
+
+
+	await ffmpeg.run('-i','video.mp4','-i','audio.mp4','-c:v','copy','-c:a','aac', 'output.mp4');
+	let file = ffmpeg.FS("readFile","output.mp4");
+	console.log(file);
+	saveAs(new File([file],'output.mp4'),'output.mp4');
+	console.log("audio and video downloaded");
 }
